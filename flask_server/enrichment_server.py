d0ea72ac0c639812d50e02e79d7b3fcf5d1d1ef0
Julian Müller <julian2.mueller@tum.de>
2024-02-13 08:53:03 +0100

    Add docker-compose.yml based on decryptE

--- flask_server/enrichment_server.py
+++ flask_server/enrichment_server.py
@@ -1,5 +1,5 @@
 # How to send a request:
-# curl -X POST -F file=@<input_file> -F session_id=ABCDEF12345  -F dataset_name=FooBar http://127.0.0.1:5000/<route>
+# curl -X POST -F file=@<input_file> -F session_id=ABCDEF12345  -F dataset_name=FooBar http://127.0.0.1:1234/<route>
 from pathlib import Path
 
 import werkzeug.wrappers
