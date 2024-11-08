7814b30efeb88c26261aa435b03740fac1a886d1
Julian Müller <julian2.mueller@tum.de>
2024-11-08 10:40:43 +0100

    Reduce number of threads, hope this will prevent ssgsea from failing

--- flask_server/gunicorn.sh
+++ flask_server/gunicorn.sh
@@ -1,1 +1,1 @@
-poetry run gunicorn enrichment_server:app  -w 1 --threads 10 -b 0.0.0.0:4321 --timeout 4000
+poetry run gunicorn enrichment_server:app  -w 1 --threads 1 -b 0.0.0.0:4321 --timeout 4000
