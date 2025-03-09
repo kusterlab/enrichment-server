74af91f0835a0244f88740cbbd8f3fd46ce63a20
Julian Müller <julian2.mueller@tum.de>
2025-03-09 10:12:14 +0100

    Cleanup

--- flask_server/enrichment_server.py
+++ flask_server/enrichment_server.py
@@ -303,29 +303,4 @@ def send_response(result: werkzeug.wrappers.Response, output_folder=None) -> fla
 
 
 if __name__ == '__main__':
-    # # DEBUG: Limit memory usage
-    # import resource
-    # import sys
-    #
-    #
-    # def memory_limit_half():
-    #     """Limit max memory usage to half."""
-    #     soft, hard = resource.getrlimit(resource.RLIMIT_AS)
-    #     # Convert KiB to bytes, and divide in two to half
-    #     resource.setrlimit(resource.RLIMIT_AS, (int(get_memory() * 1024 / 2), hard))
-    #
-    #
-    # def get_memory():
-    #     with open('/proc/meminfo', 'r') as mem:
-    #         free_memory = 0
-    #         for i in mem:
-    #             sline = i.split()
-    #             if str(sline[0]) in ('MemFree:', 'Buffers:', 'Cached:'):
-    #                 free_memory += int(sline[1])
-    #     return free_memory  # KiB
-    #
-    #
-    # # GUBED
-    # memory_limit_half()
-
     app.run(debug=os.getenv("PRODUCTION", '0') != '1', host='0.0.0.0', port=int(os.getenv("PORT", '4321')))
